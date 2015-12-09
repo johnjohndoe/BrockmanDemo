d36353928909e48d7c29f670be212ba7afc24f4c
Tobias Preuss <tobias.preuss@googlemail.com>
2015-12-09 13:45:29 +0100

    Show response failures in "offers empty" text view.

--- app/src/main/java/info/metadude/android/brockman/demo/MainActivity.java
+++ app/src/main/java/info/metadude/android/brockman/demo/MainActivity.java
@@ -1,12 +1,15 @@
 package info.metadude.android.brockman.demo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.logging.HttpLoggingInterceptor;
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mOffersRecyclerView;
 
     @Bind(R.id.offers_empty)
-    View mOffersEmptyView;
+    TextView mOffersEmptyView;
 
     private StreamsService mStreamsService;
 
@@ -49,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         mOffersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mOffersRecyclerView.setHasFixedSize(true);
-        toggleVisibility(null);
+        toggleVisibility(null, null);
         updateOffersView(null);
 
         initStreamsService();
@@ -75,25 +78,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Response<List<Offer>> response, Retrofit retrofit) {
                 if (response.isSuccess()) {
-                    onSuccess(response.body());
+                    onGetOffersResponseSuccess(response.body());
+                } else {
+                    com.squareup.okhttp.Response raw = response.raw();
+                    onGetOffersResponseFailure("HTTP " + raw.code() + ": " + raw.message());
                 }
             }
 
             @Override
             public void onFailure(Throwable t) {
-                onError(t.getMessage());
+                onCallbackFailure(t.getMessage());
                 t.printStackTrace();
             }
         });
     }
 
-    private void onSuccess(List<Offer> offers) {
+    private void onGetOffersResponseSuccess(List<Offer> offers) {
         updateOffersView(offers);
-        toggleVisibility(offers);
+        toggleVisibility(offers, null);
     }
 
-    private void onError(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    private void onGetOffersResponseFailure(@NonNull String message) {
+        toggleVisibility(null, message);
+    }
+
+    private void onCallbackFailure(String message) {
+        toggleVisibility(null, message);
     }
 
     private void updateOffersView(List<Offer> offers) {
@@ -114,9 +124,12 @@ public class MainActivity extends AppCompatActivity {
         mOffersRecyclerView.setAdapter(offersAdapter);
     }
 
-    private void toggleVisibility(List<Offer> offers) {
+    private void toggleVisibility(@Nullable List<Offer> offers, @Nullable String errorMessage) {
         if (offers == null || offers.isEmpty()) {
             mOffersRecyclerView.setVisibility(View.GONE);
+            if (!TextUtils.isEmpty(errorMessage)) {
+                mOffersEmptyView.setText(errorMessage);
+            }
             mOffersEmptyView.setVisibility(View.VISIBLE);
         } else {
             mOffersRecyclerView.setVisibility(View.VISIBLE);
