8b226d8ae8a4c47955ce357ba44857da1fd9cc14
Tobias Preuss <tobias.preuss@googlemail.com>
2018-11-22 12:51:40 +0100

    Use SDK v.27 and support library v.27.1.1.
    + Match signature of parent method.

--- app/src/main/java/info/metadude/android/brockman/demo/StreamViewModelsAdapter.java
+++ app/src/main/java/info/metadude/android/brockman/demo/StreamViewModelsAdapter.java
@@ -1,5 +1,6 @@
 package info.metadude.android.brockman.demo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,15 +18,16 @@ class StreamViewModelsAdapter extends RecyclerView.Adapter<StreamViewModelHolder
         this.streamViewModels = streamViewModels;
     }
 
+    @NonNull
     @Override
-    public StreamViewModelHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public StreamViewModelHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View streamViewItem = layoutInflater.inflate(R.layout.stream_item, parent, false);
         return new StreamViewModelHolder(streamViewItem);
     }
 
     @Override
-    public void onBindViewHolder(StreamViewModelHolder holder, int position) {
+    public void onBindViewHolder(@NonNull StreamViewModelHolder holder, int position) {
         StreamViewModel streamViewModel = streamViewModels.get(position);
         holder.bind(streamViewModel);
     }
